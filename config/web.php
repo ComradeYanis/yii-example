e64868101813d34c8b90542b13a620a0d6d93f98
ComradeYanis <ComradeYanis@github.com>
2018-04-25 20:36:06 +0300

    Add hard redirect

--- config/web.php
+++ config/web.php
@@ -55,7 +55,9 @@ $config = [
             'showScriptName' => false,
             'rules' => [
                 '' => 'site/index',                                
-                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' =>          '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ],
         ],
         
